dcde2e853449403004426b52a67046dc548c0c1f
Giacomo PIazzesi <giapiazze@gmail.com>
2017-08-07 00:02:47 +0200

    20170804 - Server Plugins, OK.

--- plugins.js
+++ plugins.js
@@ -1,36 +1,9 @@
 const AuthBasic = require('hapi-auth-basic');
 const AuthJWT = require ('hapi-auth-jwt2');
 
-const Good = require('good');
 const GoodPack = require('./plugins/good_plugin');
 // const Auth = require ('../plugins/auth');
-
-const HapiSwagger = require('hapi-swagger');
-const Inert = require('inert');
-const Vision = require('vision');
-
-
-let swaggerOptions = {
-	info: {
-		'title': 'Test API Documentation',
-		'version': '1.0.0',
-	}
-};
-
-let goodOptions = {
-	reporters: {
-		console: [{
-			module: 'good-squeeze',
-			name: 'Squeeze',
-			args: [{
-				response: '*',
-				log: '*'
-			}]
-		}, {
-			module: 'good-console'
-		}, 'stdout']
-	}
-};
+const SwaggerPack = require('./plugins/swagger_plugin');
 
 
 const Plugins = [
@@ -42,12 +15,9 @@ const Plugins = [
 	{ register: AuthJWT },
 
 	// Swagger
-	Inert,
-	Vision,
-	{
-		register: HapiSwagger,
-		options: swaggerOptions
-	},
+	SwaggerPack.inert,
+	SwaggerPack.vision,
+	SwaggerPack.swagger,
 
 	// // local auth plugin
 	// { register: Auth },
--- plugins/swagger_plugin.js
+++ plugins/swagger_plugin.js
@@ -1,34 +1,26 @@
-const HapiSwagger = require('hapi-swagger');
+const Swagger = require('hapi-swagger');
 const Inert = require('inert');
 const Vision = require('vision');
 
-const options = {
+const Options = {
 	info: {
 		'title': 'HAPI Server API Documentation',
 		'version': '1.0.0',
 	}
 };
 
-const plugin = {
-	register: (server, options, next) => {
-
-		server.register([
-				Inert,
-				Vision,
-				{
-					register: HapiSwagger,
-					options: options
-				}],
-
-			next()
-		)},
-	options: options
-};
-
-plugin.attributes = {
-	name: 'hapi-swagger',
-	version: '7.7.0',
+const SwaggerPack = {
+	inert: {
+		register: Inert,
+	},
+	vision: {
+		register: Vision,
+	},
+	swagger: {
+		register: Swagger,
+		options: Options,
+	}
 };
 
 
-module.export = plugin;
+module.exports = SwaggerPack;
