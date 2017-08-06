88c4452f028e0313b5e0ea48ab8ccfc6ca97318d
Giacomo PIazzesi <giapiazze@gmail.com>
2017-08-06 10:34:23 +0200

    20170804 - User data

--- models/user/user_routes.js
+++ models/user/user_routes.js
@@ -20,16 +20,15 @@ module.exports.register = (server, options, next) => {
 				auth: false,
 				notes: ['Return the Users list filtered by query (params) paginated. Default pageSize: 10'],
 				validate: {
-					query: UserValidations.query,
-					// query: {
-					// 	id: Joi.number().integer().min(1).description('the user ID PK increment'),
-					// 	username: Joi.string().min(3).max(64),
-					// 	is_active: Joi.boolean(),
-					// 	columns: Joi.array().items(Joi.string()),
-					// 	page: Joi.number().integer().min(1).default(1),
-					// 	pageSize: Joi.number().integer().min(10).default(10),
-					// 	count: Joi.boolean(),
-					// }
+					query: {
+						id: Joi.number().integer().min(1).description('the user ID PK increment'),
+						email: Joi.string().email().description('the user email'),
+						firstName: Joi.string().min(3).max(64),
+						lastName: Joi.string().min(3).max(64),
+						isActive: Joi.boolean(),
+						page: Joi.number().integer().min(1),
+						pageSize: Joi.number().integer().min(10)
+					}
 				}
 			},
 		}
